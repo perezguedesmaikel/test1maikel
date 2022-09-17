c60156c164150919712e02e574e6e534f94ea9a7
Maikel33 <perezguedesmaikel@gmail.com>
2022-09-17 17:50:23 -0400

    hasta ahora todo bien

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import TodoApp from "./components/TodoApp";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="container">
-      <TodoApp />
+    <div>
+      <NavBar />
+      <div className="container">
+        <TodoApp />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Button variant="contained">Test1</Button>
+            <Button variant="contained">Test2</Button>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
